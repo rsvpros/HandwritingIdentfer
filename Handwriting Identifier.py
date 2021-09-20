f3b21418d06438def7d65edade0d7b138297ec79
rsvpros <shankarro@vcu.edu>
2021-09-20 16:29:56 -0400

    Add files via upload
    .py file if you don't use Jupyter notebook

---
+++ Handwriting Identifier.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # SETUP
+
+# In[3]:
+
+
+'''Ravi Shankar HW 5 Question 1 CNN'''
+#Referenced for CNN: https://keras.io/examples/vision/mnist_convnet/
+#Referenced for data visualization: https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+
+##SETUP
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+
+# # Prepare the data
+
+# In[1]:
+
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+print(x_train[0])
+
+
+# # Build the model
+
+# In[5]:
+
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+
+# # Train the model
+
+# In[7]:
+
+
+batch_size = 128
+epochs = 15
+
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+
+# # Evaluate the trained model
+
+# In[8]:
+
+
+
+score = model.evaluate(x_test, y_test, verbose=1)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+
+#https://machinelearningmastery.com/display-deep-learning-model-training-history-in-keras/
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
